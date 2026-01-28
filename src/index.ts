12510554ad60004d8d727f6590e04aa6baf46223
Monedita <gonzalo_fder@hotmail.com.ar>
2026-01-28 15:56:26 +0100

    Change how to access env variables, add void type returns to functions and add some comments, all inside index.ts.

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,27 @@ import type { Response, Request } from "express";
 import type { Express } from "express";
 import express from "express";
 import cors from "cors";
-import { env } from "node:process";
 
 const app: Express = express();
-const port: number = env.PORT ? parseInt(env.PORT) : 3000;
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-app.use(cors(env.CORS_OPTIONS ? JSON.parse(env.CORS_OPTIONS) : {
-    origin: "*",
-    optionsSuccessStatus: 200
-}));
+// Middlewares
+app.use(cors(
+  process.env.CORS_OPTIONS
+    ? JSON.parse(process.env.CORS_OPTIONS)
+    : {
+      origin: "*",
+      optionsSuccessStatus: 200
+    }
+));
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+// Routes
+app.get("/", (req: Request, res: Response): void => {
   res.send("Server working!");
 });
 
-app.listen(port, () => {
+// Start server
+app.listen(port, (): void => {
   console.log(`Server is running at http://localhost:${port}`);
 });
