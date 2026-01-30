1b538b044cf0912b41e78956c4b22817660a565b
Monedita <gonzalo_fder@hotmail.com.ar>
2026-01-30 14:43:19 +0100

    Add convert false to options in validate method of schemas inside schemaValidator.middleware.ts in order to prevent string numbers to pass when a number is required.

--- src/middlewares/schemaValidator.middleware.ts
+++ src/middlewares/schemaValidator.middleware.ts
@@ -2,14 +2,16 @@ import type { Request, Response, NextFunction } from 'express';
 import type { Schema } from 'joi';
 
 
-export default function schemaValidator (schema: Schema, property: keyof Request = 'body') {
+export default function schemaValidator(schema: Schema, property: keyof Request = 'body') {
   return (req: Request, res: Response, next: NextFunction) => {
     const data = req[property] ?? {}; // req.body, req.params, req.query
-    const {error} = schema.validate(data, { abortEarly: false });
+    // abortEarly: false to gather all errors
+    // convert: false to avoid type coercion, e.g., string to number
+    const { error } = schema.validate(data, { abortEarly: false, convert: false });
     if (error) {
-        res.status(400).json(error.details);
+      res.status(400).json(error.details);
     } else {
-        next();
+      next();
     }
   }
 }
